5fc969c729378c5435d48c35643fb8b2aa9a582d
Dai Dung Lam <lamdaidung10c5@gmail.com>
2022-10-06 14:16:59 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var dataPrep = require("./data_prep")
+var dataPrep = require("./data_prep");
+app.use(express.static('public'));
 
 var HTTP_PORT = process.env.Port || 8080;
 
@@ -12,8 +13,6 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/home.html");
 });
 
-
-
 app.get("/GPA", (req,res) =>{
     dataPrep.cpa().then((data) =>{
         const student = data;
@@ -23,6 +22,21 @@ app.get("/GPA", (req,res) =>{
     }).catch((err) =>{
         res.send("Message: ", err);
     })
+})
+
+app.get("highGPA", (req,res) =>{
+    dataPrep.highGPA().then((data) =>{
+        const highStudent = data;
+        let resText = "<br>";
+        resText = JSON.stringify(highStudent) + "<br>";
+        res.send(resText);
+    }).catch((err) =>{
+        res.send("Message: ", err);
+    })
+})
+
+app.use((req, res) =>{
+    res.status(404).send("<b>Error 404: Page not found.</b>");
 })
 
 dataPrep.prep().then(() =>{
